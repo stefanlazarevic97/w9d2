dc4b1124c49036a0c5eba9cfe5e86c6e476335f7
stefanlazarevic <stefanlaza97@gmail.com>
2023-07-18 14:33:10 -0700

    add setUpBoard

--- browser-tic-tac-toe/src/index.js
+++ browser-tic-tac-toe/src/index.js
@@ -1,5 +1,7 @@
-Add your import statements for View and Game here
+import View from "./ttt-view.js"
+import Game from "../ttt_node/game.js"
 
 document.addEventListener("DOMContentLoaded", () => {
-  // Your code here
+    let game = new Game();
+    let view = new View(game, document.getElementsByClassName("ttt")[0]);
 });
--- browser-tic-tac-toe/src/ttt-view.js
+++ browser-tic-tac-toe/src/ttt-view.js
@@ -1,18 +1,35 @@
 class View {
-  constructor(game, el) {
-  }
-  
-  setupBoard() {
-  }
-  
-  handleClick(e) {
-  }
+    constructor(game, el) {
+        this.game = game;
+        this.el = el;
+        this.setupBoard();
+        this.handleClick(e);
+    }
+    
+    setupBoard() {
+        let ul = document.createElement('ul');
 
-  makeMove(square) {
-  }
-  
-  handleGameOver() {
-  }
+        for (let row = 0; row < 3; row++) {
+            for (let col = 0; col < 3; col++) {
+                const li = document.createElement('li');
+                li.dataset.row = row;
+                li.dataset.col = col;
+                ul.appendChild(li);
+            }
+        }
+
+        this.el.appendChild(ul);
+    }
+    
+    handleClick(e) {
+
+    }
+
+    makeMove(square) {
+    }
+    
+    handleGameOver() {
+    }
 }
 
 export default View;
